b9089b1b3b88df978b3c2083a573e4ee7aafc34e
DevGru06 <39959853+DevGru06@users.noreply.github.com>
2019-01-29 15:34:17 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+//jshint esversion:6
+const express = require("express");
+const bodyparser = require("body-parser");
+const request = require('request');
+
+const app = express();
+const port = 3000;
+
+// const apiKey = 'b80abd45550ab05ea01d393c49f1d574-us20';
+// listID ea2966df1a
+
+app.use(express.static("public"));
+
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+app.listen(port || process.env.PORT, () => {
+  console.log("Server is running on port: " + port);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
+
+
+app.post("/", (req, res) => {
+  var firstName = req.body.firstname;
+  var lastName = req.body.lastname;
+  var email = req.body.email;
+
+  var data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: 'https://us20.api.mailchimp.com/3.0/lists/ea2966df1a',
+    method: "POST",
+    headers: {
+      "Authorization": "anystring b80abd45550ab05ea01d393c49f1d574-us20"
+    },
+    body: jsonData
+  };
+
+  request(options, (error, response, body) => {
+    if (error) {
+      console.log(error);
+      // res.send("<h1>Dun Errored" + error + "</h1>");
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode === 200) {
+        console.log(response.statusCode);
+        // res.send("<h1>Success!</h1>");
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        console.log(error);
+        // res.send("<h1>Dun Errored " + error + "</h1>");
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+});
